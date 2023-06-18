7329a666946ab6cc802d5aa5e3ccc0e682b79753
A1serious1programmer <114296963+A1serious1programmer@users.noreply.github.com>
2023-06-19 01:34:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+
 document.querySelectorAll(".nav")[0].addEventListener("click", function(){
   window.scroll({
   top: 800,
@@ -31,5 +32,9 @@ document.querySelectorAll(".nav")[4].addEventListener("click", function(){
 });
 
 document.querySelector(".menu_button").addEventListener("click", function(){
-  document.querySelector(".phone_nav").classList.toggle("hide");
+  document.querySelector(".phone_nav").classList.add("ani");
+  document.querySelector(".phone_nav").classList.remove("hide");
+})
+document.querySelector("#cross").addEventListener("click", function(){
+  document.querySelector(".phone_nav").classList.add("hide");
 })
