f6821658aeb0f345b845d8c4bdbb25938f23edd7
A1serious1programmer <114296963+A1serious1programmer@users.noreply.github.com>
2023-06-19 22:28:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ document.querySelectorAll(".nav")[0].addEventListener("click", function(){
 
 document.querySelectorAll(".nav")[1].addEventListener("click", function(){
   window.scroll({
-  top: 1900,
+  top: 2000,
   left: 0,
   behavior: 'smooth'
 });
 });
 document.querySelectorAll(".nav")[3].addEventListener("click", function(){
   window.scroll({
-  top: 800,
+  top: 600,
   left: 0,
   behavior: 'smooth'
 });
 });
 document.querySelectorAll(".nav")[4].addEventListener("click", function(){
   window.scroll({
-  top: 1600,
+  top: 1400,
   left: 0,
   behavior: 'smooth'
 });
